9dd2b22b0bcb4235940540ffa77087bf3da62f32
eerzho <eerzho@gmail.com>
2020-04-12 19:47:53 +0600

    Finish

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -13,16 +13,16 @@ namespace generatorID.Controllers
         [HttpPost]
         public IActionResult Index(string student_id)
         {
-            // string authData = $"Login: {login}   Password: {password}";
             if (Database.shared?.students != null)
                 foreach (var student in Database.shared.students)
                 {
-                    string gennerator_id = Convert.ToString(student.FullName[0]);
+                    string gennerator_id = "";
+                    gennerator_id = Convert.ToString(student.FullName[0]);
                     string per = Convert.ToString(student.FullName[student.FullName.Length - 1]).ToUpper();
                     gennerator_id += per;
                     gennerator_id += Convert.ToString(student.FullName.Length);
-                    gennerator_id += student.DOB[student.DOB.Length - 1];
                     gennerator_id += student.DOB[student.DOB.Length - 2];
+                    gennerator_id += student.DOB[student.DOB.Length - 1];
                     gennerator_id += student.Group[student.Group.Length - 4];
                     gennerator_id += student.Group[student.Group.Length - 2];
                     if (gennerator_id.Equals(student_id))
